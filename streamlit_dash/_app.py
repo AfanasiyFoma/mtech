bae7abff6682c50479e6d883a05f32a4c29c9766
Афанасий Булгаков <111143010+AfanasiyFoma@users.noreply.github.com>
2023-12-17 00:07:17 +0300

    Update _app.py

--- streamlit_dash/_app.py
+++ streamlit_dash/_app.py
@@ -8,3 +8,5 @@ st.title("Hello GeeksForGeeks !!!")
 data1 = pd.read_csv('C:/openData/Mtech/2/file1.csv')
 
 print(data1.head())
+
+st.bar_chart(data=data.groupby('Пол')['Возраст'], x='Пол', y='Возраст', use_container_width=True)
